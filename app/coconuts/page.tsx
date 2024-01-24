488d5c944149a5b6e7d3e2f4b4debebafeed4fc4
Dan Fyfe <fyfedaniel@gmail.com>
2024-01-24 17:13:57 -0500

    more build issues

--- app/coconuts/page.tsx
+++ app/coconuts/page.tsx
@@ -6,14 +6,14 @@ interface Page {
 };
 
 const Coconuts = async () => {
-    const res = await fetch('http://127.0.0.1:8000/coconuts');
-    const pages = await res.json();
+    // const res = await fetch('http://127.0.0.1:8000/coconuts');
+    // const pages = await res.json();
 
     return(
         <>
             <div className="border-2">
                 {
-                    pages?.map((page: Page, idx: number) => {
+                    []?.map((page: Page, idx: number) => {
                         return (
                             <Coconut slug={page.slug} key={`slug-${idx}`}/>
                         )
