0abd9b32c900bdd38320e4f701bd7a0af9eb33d0
Dan Fyfe <fyfedaniel@gmail.com>
2024-02-16 10:00:26 -0500

    quick look at api

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,3 +1,11 @@
 export async function GET(request: Request) {
   //
+  const res = await fetch('http://localhost:8080/api/v1/posts', {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+
+  const data = await res.json();
+  return Response.json({ data });
 }
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -2,9 +2,7 @@ import H1 from "@/components/core/typography/H1";
 import { createClient } from '@supabase/supabase-js';
 
 const BlogIndex = async () => {
-  const supabase = createClient(process.env.SUPABASE_URL ?? '', process.env.SUPABASE_KEY ?? '', {
-    global: { fetch: fetch.bind(globalThis) }
-  });
+  // const data = await fetch('http://localhost:3001/api/posts');
   return (
     <H1 className="self-start">Here are some thoughts</H1>
   )
