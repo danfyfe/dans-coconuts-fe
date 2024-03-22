262a1902de63a6a6930f1519d127484bc39a8854
Dan Fyfe <fyfedaniel@gmail.com>
2024-03-22 14:05:27 -0400

    add coconuts route and update rain opacity

---
+++ app/coconuts/page.tsx
@@ -0,0 +1,14 @@
+import LandscapeContainer from '@/components/core/containers/landscape-container';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Dan's Coconuts",
+  description: 'Just a beach...'
+}
+ 
+
+const CoconutsPage = () => (
+  <LandscapeContainer />
+);
+
+export default CoconutsPage;
