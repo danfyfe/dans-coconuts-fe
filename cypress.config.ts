c1dee6cb32235cdd4ca79314dc9462f8e1c8c731
Dan Fyfe <fyfedaniel@gmail.com>
2024-03-29 10:09:25 -0400

    update config

---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  projectId: "3u3goe",
+
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    // setupNodeEvents(on, config) {
+    //   // implement node event listeners here
+    // },
+  },
+};
