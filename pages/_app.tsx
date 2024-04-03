eedc9149b71914ed252a94d8158083ba4ff8f41e
Dan Fyfe <fyfedaniel@gmail.com>
2024-04-03 16:21:50 -0400

    test dotenv

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,7 @@
 import type { AppProps } from 'next/app'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
