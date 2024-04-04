6e87af93a280df5f6be7a5fbef4a719c3124789d
Dan Fyfe <fyfedaniel@gmail.com>
2024-04-04 11:34:56 -0400

    update formating

--- app/github/github-content.tsx
+++ app/github/github-content.tsx
@@ -5,11 +5,11 @@ import P from "@/components/core/typography/P";
 
 const GitHubContent = ({ children }: { children: ReactNode }) => (
   <ContentContainer className="flex flex-col w-3/4 md:w-1/4">
-        <H1 className="mb-2">GitHub Links</H1>
-        <P className="text-lg mb-4">Here are the links to the repositories used in this project</P>
-          { children }
-        <P className="text-sm mt-4">This page is built using the GitHub GraphQL endpoint</P>
-      </ContentContainer>
+    <H1 className="mb-2">GitHub Links</H1>
+    <P className="text-lg mb-4">Here are the links to the repositories used in this project</P>
+      { children }
+    <P className="text-sm mt-4">This page is built using the GitHub GraphQL endpoint</P>
+  </ContentContainer>
 );
 
 export default GitHubContent;
