5d9b637dec67a2bff278c3b8757a4185346f650f
Dan Fyfe <fyfedaniel@gmail.com>
2024-04-11 12:20:02 -0400

    add description in query

--- graphql/github/queries.ts
+++ graphql/github/queries.ts
@@ -2,11 +2,12 @@ export const GetGitHubRepos = `
   query GetGitHubRepos {
     user(login: "danfyfe") {
       name
-      pinnedItems(first: 3, types: REPOSITORY) {
+      pinnedItems(first: 4, types: REPOSITORY) {
         nodes {
           ...on Repository {
             name
             url
+            description
             updatedAt
           }
         }
