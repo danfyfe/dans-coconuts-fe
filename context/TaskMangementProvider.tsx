442b672a7ee62b084d36c74d0f1575e57dcf4f95
Dan Fyfe <fyfedaniel@gmail.com>
2024-05-16 17:27:26 -0400

    start types for active form

--- context/TaskMangementProvider.tsx
+++ context/TaskMangementProvider.tsx
@@ -5,24 +5,28 @@ interface IOrganization {
   title: string
 }
 
-type ActiveForm = 'organization' | null;
+type ActiveTaskManagementForm = 'organization' | 'project' | 'task' | null;
 
 interface ITaskManagementProvider {
   organizations: IOrganization[];
-  setOrganizations: Dispatch<SetStateAction<IOrganization[]>>
+  setOrganizations: Dispatch<SetStateAction<IOrganization[]>>;
+  activeForm: ActiveTaskManagementForm;
+  setActiveForm: Dispatch<SetStateAction<ActiveTaskManagementForm>>;
 }
 
 export const TaskManagementContext = createContext<ITaskManagementProvider>({
   organizations: [],
-  setOrganizations: () => {}
+  setOrganizations: () => {},
+  activeForm: null,
+  setActiveForm: () => {}
 });
 
 export const TaskManagementProvider = ({ children }: { children: React.ReactNode }) => {
   const [organizations, setOrganizations] = useState<IOrganization[]>([]);
-  const [activeForm, setActiveForm] = useState<ActiveForm>(null);
+  const [activeForm, setActiveForm] = useState<ActiveTaskManagementForm>(null);
 
   return (
-    <TaskManagementContext.Provider value={{ organizations, setOrganizations }}>
+    <TaskManagementContext.Provider value={{ organizations, setOrganizations, activeForm, setActiveForm }}>
       {children}
     </TaskManagementContext.Provider>
   )
