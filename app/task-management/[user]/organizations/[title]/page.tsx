e3831557e066bd2e335557153ee775658e664f93
Dan Fyfe <fyfedaniel@gmail.com>
2024-05-21 20:55:30 -0400

    fix org page

--- app/task-management/[user]/organizations/[title]/page.tsx
+++ app/task-management/[user]/organizations/[title]/page.tsx
@@ -1,18 +1,12 @@
 import Organization from "@/components/task-management/organization";
-import { redirect } from "next/navigation";
 
 const OrganizationPage = ({ params }: { params: { title: string; } }) => {
   const { title } = params;
   const orgTitle = decodeURIComponent(title);
-  // get organization
-  const organization = [].find((org) => org === orgTitle);
-  if (!organization) {
-    redirect('/')
-  }
 
   return (
     <>
-      <Organization organization={organization} />
+      <Organization orgTitle={orgTitle} />
     </>
   )
 };
--- components/task-management/organization/index.tsx
+++ components/task-management/organization/index.tsx
@@ -1,16 +1,24 @@
+'use client'
 import ContentContainer from "@/components/core/containers/content-container";
-import { IOrganization } from "@/interfaces/task-management";
 import TaskManagementHeader from "../task-management-header";
 import H2 from "@/components/core/typography/H2";
+import { redirect } from "next/navigation";
+import { TaskManagementContext } from "@/context/TaskManagementProvider";
+import { useContext } from "react";
 
+const Organization = ({ orgTitle }: { orgTitle: string }) => {
+  const { state } = useContext(TaskManagementContext);
+    // get organization
+    const organization = state.organizations?.find((org) => org.title === orgTitle);
 
-
-
-const Organization = ({ organization }: { organization: IOrganization }) => {
+    if (!organization) {
+      redirect('/')
+    }
+  
   return (
     <ContentContainer className="md:w-5/6 md:py-2 md:px-4">
       <TaskManagementHeader />
-      <H2 className="!text-left text-xl">{organization.title}</H2>
+      <H2 className="!text-left text-2xl">{organization.title}</H2>
     </ContentContainer>
   )
 };
