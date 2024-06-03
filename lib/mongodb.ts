7fe2e11f0d69c9f00140b1f3c37bbc7aa806d893
Dan Fyfe <fyfedaniel@gmail.com>
2024-06-03 16:09:01 -0400

    remove connection check - was keeping search index from working

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -6,7 +6,11 @@ const connection: { isConnected?: number } = {};
 const dbURI = process.env.MONGODB_URI ?? '';
 
 async function connectMongoDB() {
-  if (connection.isConnected) return;
+  // TODO: look into why this is causing an issue with search input for users
+  // if (connection.isConnected) {
+  //   console.log('Still connected to database.');
+  //   return
+  // };
 
   try {
     const db = await mongoose.connect(dbURI, {
@@ -14,6 +18,7 @@ async function connectMongoDB() {
     });
     connection.isConnected = db.connections[0].readyState;
     console.log('Connected to database.')
+    return db;
   } catch (error) {
     const message = getErrorMessage(error);
     console.log('There was an error connecting to the database.', message)
