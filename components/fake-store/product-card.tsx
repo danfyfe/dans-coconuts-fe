3f338a103815e2eec103284c34a543c3fdab79cc
Dan Fyfe <fyfedaniel@gmail.com>
2024-06-04 17:43:35 -0400

    cont product card

--- components/fake-store/product-card.tsx
+++ components/fake-store/product-card.tsx
@@ -1,14 +1,37 @@
+import { formatUSD } from "@/lib/formatting/formatUSD";
 import H3 from "../core/typography/H3";
 import P from "../core/typography/P";
 import { Product } from "./types";
+import Image from "next/image";
 
 const ProductCard = ({
-  title, price, description, category, image, rating
+  title, price, category, image, rating
 }: Partial<Product>) => {
   return (
     <article className="w-auto">
       <H3>{title}</H3>
-      <P>${price}</P>
+      <P className="capitalize">{category}</P>
+      <P>{!!price ? formatUSD(price): ''}</P>
+      {
+        !!title && !!image ? (
+          <div>
+            <Image 
+              height={50}
+              width={50}
+              src={image}
+              alt={title}
+            />
+          </div>
+        ): null
+      }
+        {
+          !!rating?.rate && !!rating?.count ? (
+            <div>
+              <P>Rating: {rating.rate}</P>
+              <P>{rating.count} ratings</P>
+            </div>
+          ) : null
+        }
     </article>
   )
 };
