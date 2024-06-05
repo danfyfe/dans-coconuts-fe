dba5c71f544ef2e84e7db262fc9c8c362216aa51
Dan Fyfe <fyfedaniel@gmail.com>
2024-06-05 11:05:59 -0400

    add meta data to fake-store

--- app/fake-store/page.tsx
+++ app/fake-store/page.tsx
@@ -1,5 +1,10 @@
-import ContentContainer from "@/components/core/containers/content-container";
+import { Metadata } from "next";
 import FakeStore from "@/components/fake-store";
+
+export const metadata: Metadata = {
+  title: "Dan's Coconuts | Fake Store",
+  description: "A Fake store at Dan's Coconuts"
+}
 
 const FakeStorePage = () => {
   return (
