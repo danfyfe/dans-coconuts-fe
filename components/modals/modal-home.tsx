06546a253b92fa8387995983cdd1d4638802b561
Dan Fyfe <fyfedaniel@gmail.com>
2024-06-11 18:12:35 -0400

    clean up

--- components/modals/modal-home.tsx
+++ components/modals/modal-home.tsx
@@ -1,12 +1,6 @@
 'use client'
-import ContentContainer from "../core/containers/content-container";
-import withModal from "@/hoc/display/withModal";
 import P from "../core/typography/P";
-import Button from "../core/utility/button";
-import { FaPlus } from "react-icons/fa";
-import { Dispatch, SetStateAction } from "react";
 import H2 from "../core/typography/H2";
-import setCookie from "@/lib/setCookie";
 import ModalUtils from "../core/modal/modal-utils";
 
 const HomePageModal = () => {
