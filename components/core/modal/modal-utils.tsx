20a7e87b702ffcfba705b1e01db114cfc62cebbc
Dan Fyfe <fyfedaniel@gmail.com>
2024-06-11 18:06:02 -0400

    make modal utils reusable

--- components/core/modal/modal-utils.tsx
+++ components/core/modal/modal-utils.tsx
@@ -1,5 +1,54 @@
-const ModalUtils = () => {
+'use client'
+import { Dispatch, ReactNode, SetStateAction } from "react";
+import setCookie from "@/lib/setCookie";
+import { FaPlus } from "react-icons/fa";
+import withModal from "@/hoc/display/withModal";
+import ContentContainer from "@/components/core/containers/content-container";
+import Button from "@/components/core/utility/button";
 
+const ModalUtils = ({ type, setOpen, children }:
+  { 
+    type: string;
+    // setOpen is supplied by withModal
+    setOpen?: Dispatch<SetStateAction<boolean>>;
+    children: ReactNode;
+  }) => {
+
+  const handleSetCookie = () => {
+    const cookieName = `${type}-modal-opt-out`;
+    setCookie(cookieName, 1);
+    if (setOpen) setOpen(false);
+  };
+
+  return (
+    <ContentContainer>
+      <Button
+        asLink
+        className="absolute text-2xl right-0 top-0 p-4"
+        onClick={() => setOpen ? setOpen(false) : {}}
+      >
+        <FaPlus className="rotate-45"/>
+      </Button>
+
+      <div>
+        {children}
+      </div>
+    
+      <div className="mt-2 grid w-full lg:grid-cols-2 lg:gap-10 gap-4">
+        <Button
+        className=""
+          onClick={handleSetCookie}
+        >
+          Don&apos;t Show Me This Again
+        </Button>
+        <Button
+          className=""
+        >
+          Start Tour
+        </Button>
+      </div>
+    </ContentContainer>
+  )
 };
 
-export default ModalUtils;
+export default withModal(ModalUtils);
--- components/modals/modal-home.tsx
+++ components/modals/modal-home.tsx
@@ -7,44 +7,19 @@ import { FaPlus } from "react-icons/fa";
 import { Dispatch, SetStateAction } from "react";
 import H2 from "../core/typography/H2";
 import setCookie from "@/lib/setCookie";
+import ModalUtils from "../core/modal/modal-utils";
 
-const HomePageModal = ({ setOpen } : { setOpen?: Dispatch<SetStateAction<boolean>> }) => {
-  const handleSetCookie = () => {
-    setCookie('homepage-modal-opt-out', 1);
-    if (setOpen) setOpen(false);
-  };
+const HomePageModal = () => {
 
   return (
-    <ContentContainer>
-        <Button
-          asLink
-          className="absolute text-2xl right-0 top-0 p-4"
-          onClick={() => setOpen ? setOpen(false) : {}}
-        >
-          <FaPlus className="rotate-45"/>
-        </Button>
-        <div>
-          <H2>Welcome to Dan&apos;s Coconuts!</H2>
-          <P>Descriptive text</P>
-          <P>Descriptive text</P>
-          <P>Descriptive text</P>
-          <P>Descriptive text</P>
-          <div className="mt-2 grid w-full lg:grid-cols-2 lg:gap-10 gap-4">
-            <Button
-            className=""
-              onClick={handleSetCookie}
-            >
-              Don&apos;t Show Me This Again
-            </Button>
-            <Button
-              className=""
-            >
-              Start Tour
-            </Button>
-          </div>
-        </div>
-    </ContentContainer>
+    <ModalUtils type="homepage">
+      <H2>Welcome to Dan&apos;s Coconuts!</H2>
+      <P>Descriptive text</P>
+      <P>Descriptive text</P>
+      <P>Descriptive text</P>
+      <P>Descriptive text</P>
+    </ModalUtils>
   )
 };
 
-export default withModal(HomePageModal);
+export default HomePageModal
