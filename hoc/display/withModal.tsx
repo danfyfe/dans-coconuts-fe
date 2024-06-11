2859fc851b655c7bf3386fa3e9ef0f0ca9e744fa
Dan Fyfe <fyfedaniel@gmail.com>
2024-06-11 17:13:16 -0400

    make modal HOC

---
+++ hoc/display/withModal.tsx
@@ -0,0 +1,20 @@
+import Modal from "@/components/core/modal";
+import { ModalContext } from "@/context/ModalProvider";
+import { ComponentType, useContext } from "react";
+
+const withModal = <P extends object>(Component: ComponentType<P>) => {
+  const ComponentWithModal = (props: P) => {
+    const { open, setOpen } = useContext(ModalContext);
+
+    return (
+      <>
+        <Modal open={open} setOpen={setOpen}>
+          <Component {...props} />
+        </Modal>
+      </>
+    )
+  }
+  return ComponentWithModal;
+};
+
+export default withModal;
