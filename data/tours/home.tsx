a1ad3b8a4f623f1c42405f5546e500acbb225e06
Dan Fyfe <fyfedaniel@gmail.com>
2024-06-17 09:30:31 -0400

    start main home page tour

---
+++ data/tours/home.tsx
@@ -0,0 +1,32 @@
+const HomeTourData = {
+  type: 'home',
+  main: {
+    title: "Welcome to Dan's Coconuts!",
+    copyElems: [
+      <>Dan&apos;s Coconuts is a beach scene at its core.</>,
+      <>Used as a kind of portfolio and learning space, it contains various exploratory projects.</>,
+      <>Take the home page tour for a basic a run through.</>,
+      <>Or enjoy the beach, built entirely using CSS animations via Tailwind.</>
+    ]
+  },
+  steps: [
+    {
+      title: 'Main Navigation',
+      copyElems: [
+        <>Clicking this coconut opens the main navigation, which contains links to other pages.</>,
+      ],
+      nextElemId: 'weather-widget-btn'
+    },
+    {
+      title: 'Weather Widget',
+      copyElems: [
+        <>Clicking the weather icon opens the Weather Widget.</>,
+        <>The Weather Widget uses the National Weather Service API to display the current weather in Miami Beach, FL.</>,
+        <>It also holds the weather toggle, which switches from a sunny day to rain. Click the cloud icon to activate.</>
+      ],
+      nextElemId: null
+    }
+  ]
+};
+
+export default HomeTourData;
