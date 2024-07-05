0e74fb4bed2c846df4b46349fa8a49566a667381
Dan Fyfe <fyfedaniel@gmail.com>
2024-07-05 16:04:10 -0400

    comment out type issues with nightwatch

--- nightwatch/ecosia.ts
+++ nightwatch/ecosia.ts
@@ -1,18 +1,18 @@
-describe('Ecosia.org Demo', function() {
-  this.tags = ['demo'];
+// describe('Ecosia.org Demo', function() {
+//   this.tags = ['demo'];
 
-  before(browser => browser.navigateTo('https://www.ecosia.org/'));
+//   before(browser => browser.navigateTo('https://www.ecosia.org/'));
 
-  it('Demo test ecosia.org', function(browser) {
-    browser
-      .waitForElementVisible('body')
-      .assert.titleContains('Ecosia')
-      .assert.visible('input[type=search]')
-      .setValue('input[type=search]', 'nightwatch')
-      .assert.visible('button[type=submit]')
-      .click('button[type=submit]')
-      .assert.textContains('.layout__content', 'Nightwatch.js');
-  });
+//   it('Demo test ecosia.org', function(browser) {
+//     browser
+//       .waitForElementVisible('body')
+//       .assert.titleContains('Ecosia')
+//       .assert.visible('input[type=search]')
+//       .setValue('input[type=search]', 'nightwatch')
+//       .assert.visible('button[type=submit]')
+//       .click('button[type=submit]')
+//       .assert.textContains('.layout__content', 'Nightwatch.js');
+//   });
 
-  after(browser => browser.end());
-});
+//   after(browser => browser.end());
+// });
--- nightwatch/mobile-app-tests/wikipedia-ios.ts
+++ nightwatch/mobile-app-tests/wikipedia-ios.ts
@@ -1,25 +1,25 @@
-import {NightwatchAPI} from 'nightwatch';
+// import {NightwatchAPI} from 'nightwatch';
 
-describe('Wikipedia iOS app test', function() {
-  before(function(app: NightwatchAPI) {
-    app.click('xpath', '//XCUIElementTypeButton[@name="Skip"]');
-  });
+// describe('Wikipedia iOS app test', function() {
+//   before(function(app: NightwatchAPI) {
+//     app.click('xpath', '//XCUIElementTypeButton[@name="Skip"]');
+//   });
 
-  it('Search for BrowserStack', async function(app: NightwatchAPI) {
-    // click on search bar at the top.
-    const searchField = app.element(by.xpath('//XCUIElementTypeSearchField[@name="Search Wikipedia"]'));
-    searchField.click();
+//   it('Search for BrowserStack', async function(app: NightwatchAPI) {
+//     // click on search bar at the top.
+//     const searchField = app.element(by.xpath('//XCUIElementTypeSearchField[@name="Search Wikipedia"]'));
+//     searchField.click();
 
-    // above step will open another search field (with same xpath), type 'browserstack' in that.
-    const editableSearchField = app.element(by.xpath('//XCUIElementTypeSearchField[@name="Search Wikipedia"]'));
-    editableSearchField.sendKeys('browserstack');
+//     // above step will open another search field (with same xpath), type 'browserstack' in that.
+//     const editableSearchField = app.element(by.xpath('//XCUIElementTypeSearchField[@name="Search Wikipedia"]'));
+//     editableSearchField.sendKeys('browserstack');
 
-    // click on search result with name attribute as 'BrowserStack'.
-    const bstackSearchResult = app.element(by.xpath('//XCUIElementTypeStaticText[@name="BrowserStack"]'));
-    bstackSearchResult.click();
+//     // click on search result with name attribute as 'BrowserStack'.
+//     const bstackSearchResult = app.element(by.xpath('//XCUIElementTypeStaticText[@name="BrowserStack"]'));
+//     bstackSearchResult.click();
 
-    // assert the title of the opened page.
-    const pageTitle = app.element(by.xpath('(//XCUIElementTypeStaticText[@name="BrowserStack"])[1]'));
-    pageTitle.getText().assert.equals('BrowserStack');
-  });
-});
+//     // assert the title of the opened page.
+//     const pageTitle = app.element(by.xpath('(//XCUIElementTypeStaticText[@name="BrowserStack"])[1]'));
+//     pageTitle.getText().assert.equals('BrowserStack');
+//   });
+// });
